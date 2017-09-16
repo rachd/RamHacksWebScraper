728afe0d95692b63a84afaf741bb42a8045b92cc
Rachel Dorn <rachel.m.dorn@gmail.com>
2017-09-16 19:06:23 -0400

    scrape all pages and remove html characters

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(bodyParser.json());
 app.post('/', function(req, res){
 	const url = req.body.url;
 	let questions = {};
+	let categories = [];
+	let promises = [];
 
 	const options = {
 	  uri: url,
@@ -18,26 +20,52 @@ app.post('/', function(req, res){
 	};
 
 	rp(options)
-	  .then(($) => {
-	    $('h4').each(function(i, elem) {
-	    	let current = $(this);
-	    	const question = current.text();
-	    	let answer = "";
-	    	current = current.next();
-	    	while(!current.hasClass("toplink")) {
-	    		answer += current.text();
-	    		current = current.next();
-	    	}
-	    	questions[question] = answer;
-	    });
-	    res.json(questions);
-	  })
-	  .catch((err) => {
-	    console.log(err);
-	  });
-
-	// res.json(url);
-
+	.then(($) => {
+		$('h3').each(function(i, elem) {
+			categories.push(url + "/" + $(this).children().first().attr('href'));
+		});
+		for (let category of categories) {
+			promises.push(new Promise((resolve, reject) => {
+				const newOptions = {
+					uri: category,
+					transform: (body) => {
+						return cheerio.load(body);
+					}
+				};
+				rp(newOptions)
+				.then(($) => {
+					let theseQuestions = {};
+					$('h4').each(function(i, elem) {
+						let current = $(this);
+						const question = current.text();
+						let answer = "";
+						current = current.next();
+						while(!current.hasClass("toplink")) {
+							answer += current.text().replace(/[\n\t\r]/g,"");
+							current = current.next();
+						}
+						theseQuestions[question] = answer;
+						resolve(theseQuestions);
+					});
+				})
+				.catch((err) => {
+					console.log(err);
+					reject();
+				});
+			}));
+		}
+		Promise.all(promises)
+		.then(values => { 
+			res.json(values.reduce((a, b) => {
+					return Object.assign(a, b);
+				})
+			);
+		})
+		.catch(err => console.log(err));
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 });
 
 app.listen(8080, () => console.log("listening on port 8080"));
