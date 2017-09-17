ed907c35c509c0582b5fff93a214da01c2f819fd
Rachel Dorn <rachel.m.dorn@gmail.com>
2017-09-16 20:57:16 -0400

    test

--- index.js
+++ index.js
@@ -20,53 +20,55 @@ app.post('/', function(req, res){
 	  }
 	};
 
-	rp(options)
-	.then(($) => {
-		$('h3').each(function(i, elem) {
-			categories.push(url + "/" + $(this).children().first().attr('href'));
-		});
-		for (let category of categories) {
-			promises.push(new Promise((resolve, reject) => {
-				const newOptions = {
-					uri: category,
-					transform: (body) => {
-						return cheerio.load(body);
-					}
-				};
-				rp(newOptions)
-				.then(($) => {
-					let theseQuestions = {};
-					$('h4').each(function(i, elem) {
-						let current = $(this);
-						const question = current.text();
-						let answer = "";
-						current = current.next();
-						while(!current.hasClass("toplink")) {
-							answer += current.text().replace(/[\n\t\r]/g,"");
-							current = current.next();
-						}
-						theseQuestions[question] = answer;
-						resolve(theseQuestions);
-					});
-				})
-				.catch((err) => {
-					console.log(err);
-					reject();
-				});
-			}));
-		}
-		Promise.all(promises)
-		.then(values => { 
-			res.json(values.reduce((a, b) => {
-					return Object.assign(a, b);
-				})
-			);
-		})
-		.catch(err => console.log(err));
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+	res.json({"hi": "hello"});
+
+	// rp(options)
+	// .then(($) => {
+	// 	$('h3').each(function(i, elem) {
+	// 		categories.push(url + "/" + $(this).children().first().attr('href'));
+	// 	});
+	// 	for (let category of categories) {
+	// 		promises.push(new Promise((resolve, reject) => {
+	// 			const newOptions = {
+	// 				uri: category,
+	// 				transform: (body) => {
+	// 					return cheerio.load(body);
+	// 				}
+	// 			};
+	// 			rp(newOptions)
+	// 			.then(($) => {
+	// 				let theseQuestions = {};
+	// 				$('h4').each(function(i, elem) {
+	// 					let current = $(this);
+	// 					const question = current.text();
+	// 					let answer = "";
+	// 					current = current.next();
+	// 					while(!current.hasClass("toplink")) {
+	// 						answer += current.text().replace(/[\n\t\r]/g,"");
+	// 						current = current.next();
+	// 					}
+	// 					theseQuestions[question] = answer;
+	// 					resolve(theseQuestions);
+	// 				});
+	// 			})
+	// 			.catch((err) => {
+	// 				console.log(err);
+	// 				reject();
+	// 			});
+	// 		}));
+	// 	}
+	// 	Promise.all(promises)
+	// 	.then(values => { 
+	// 		res.json(values.reduce((a, b) => {
+	// 				return Object.assign(a, b);
+	// 			})
+	// 		);
+	// 	})
+	// 	.catch(err => console.log(err));
+	// })
+	// .catch((err) => {
+	// 	console.log(err);
+	// });
 });
 
 app.listen(8080, () => console.log("listening on port 8080"));
